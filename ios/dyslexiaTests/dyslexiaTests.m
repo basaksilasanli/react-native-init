30e80be2e309e3dd72c73cca2b0f8ec7fcd4664d
basaksilasanli <basaksilasanli@gmail.com>
2019-06-01 14:54:31 +0300

    proje olusturuldu

--- ios/dyslexiaTests/dyslexiaTests.m
+++ ios/dyslexiaTests/dyslexiaTests.m
@@ -14,11 +14,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React Native!"
 
-@interface initialReactNativeProjectTests : XCTestCase
+@interface dyslexiaTests : XCTestCase
 
 @end
 
-@implementation initialReactNativeProjectTests
+@implementation dyslexiaTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
